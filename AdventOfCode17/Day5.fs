0b3861d97fa7957d28ebb6b3cf3ab10899849821
Tim Stephenson <tim.stephenson@moneycorp.com>
2017-12-07 09:26:07 +0000

    days 5 and 6

---
+++ AdventOfCode17/Day5.fs
@@ -0,0 +1,35 @@
+﻿module Day5
+
+    open System
+    open System.IO
+
+    let private input =
+        let path = __SOURCE_DIRECTORY__ + "\Day5Input.txt"
+        path 
+        |> File.ReadAllLines
+        |> Array.map (Int32.Parse)
+
+    let rec private jump(count, loc, arr: int []) =
+        if loc >= arr.Length then count+1
+        else
+            let cmd = arr.[loc]
+            let next = loc + cmd
+            arr.[loc] <- cmd + 1
+            jump(count+1, next, arr)
+
+    let rec private jumpOffset(count, loc, arr: int []) =
+        if loc >= arr.Length then count+1
+        else
+            let cmd = arr.[loc]
+            let next = loc + cmd
+            if cmd >=3 then arr.[loc] <- cmd - 1
+            else arr.[loc] <- cmd + 1
+            jumpOffset(count+1, next, arr)
+
+    let answer() =
+        let ans = jump(0, input.[0], input)
+        ans
+
+    let answer2 =
+        let ans = jumpOffset(0, input.[0], input)
+        ans
---
+++ AdventOfCode17/Day6.fs
@@ -0,0 +1,27 @@
+﻿module Day6
+
+    let testInput = [|0; 2; 7; 0|]
+    let private input = [|14; 0; 15; 12; 11; 11; 3; 5; 1; 6; 8; 4; 9; 1; 8; 4|]
+
+    let rec private nextList(allLists, curr, count) =
+        let newList = curr |> Array.toList
+        if allLists |> List.contains newList then (count, newList)
+        else 
+            let newAllLists = newList::allLists
+            let max = curr |> Array.max
+            let mutable pos = curr |> Array.findIndex (fun e -> e = max)
+            curr.[pos] <- 0
+            for _ in 1..max do
+                pos <- pos+1
+                if pos > curr.Length-1 then pos <- 0
+                curr.[pos] <- curr.[pos] + 1
+            nextList(newAllLists, curr, count+1)
+
+
+    let answer =
+        let ansC, ansL = nextList([], input, 0)
+        ansL
+
+    let answer2 = 
+        let ansC, ansL = nextList([], answer |> List.toArray, 0)
+        ansC
