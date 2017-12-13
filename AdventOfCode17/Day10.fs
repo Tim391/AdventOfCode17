2b419fd1aa4cd55c4537077118ec812a6d20a5f4
Tim Stephenson <tim.stephenson@moneycorp.com>
2017-12-13 14:05:44 +0000

    10 and 11

--- AdventOfCode17/Day10.fs
+++ AdventOfCode17/Day10.fs
@@ -9,8 +9,41 @@
         |> Array.map int
         |> Array.toList
 
+    type State = { position: int; skip: int; list: int list }
+
+    let newPosition(position, length, listlength) =
+        let newPos = position + length
+        if newPos > listlength-1 then newPos - listlength
+        else newPos
+
+    let reverse current length =
+        let splitAt = current.position + length
+        if splitAt > current.list.Length then 
+            let (h, t) = current.list |> List.splitAt current.position
+            let tailSize = t.Length
+            let workingList = List.append t h
+            let (rh, rt) = workingList |> List.splitAt length
+            let reversed = rh |> List.rev
+            let b = List.append reversed rt 
+            let (bh, bt) = b |> List.splitAt tailSize
+            let newList = List.append bt bh
+            { position = newPosition(current.position, length+current.skip, current.list.Length); skip = current.skip + 1; list = newList }
+        else
+            let (h, t) = current.list |> List.splitAt splitAt
+            let workingPos = current.position + t.Length
+            let workingList = List.append t h
+            let (rh, rt) = workingList |> List.splitAt workingPos
+            let reversed = rt |> List.rev
+            let b = List.append rh reversed
+            let (bh, bt) = b |> List.splitAt t.Length
+            let newList = List.append bt bh
+            { position = newPosition(current.position, length+current.skip, current.list.Length); skip = current.skip + 1; list = newList }
 
     let answer = 
-        let initialList = [ 0..255 ]
-        input
+        let ans =
+            input
+            |> List.fold reverse { position = 0; skip = 0; list = [ 0..255 ] }
+        ans.list
+        |> List.take 2
+        |> List.fold (fun a e -> e * a) 1
 
---
+++ AdventOfCode17/Day11.fs
@@ -0,0 +1,39 @@
+﻿module Day11
+
+    open System.IO
+    open System
+
+    let private input = 
+        let path = __SOURCE_DIRECTORY__ + "\Day11Input.txt"
+        let str = path |> File.ReadAllText
+        str.Split(',')
+        |> Array.toList
+
+    let nextCoord coord dir =
+        let (x, y, z) = coord
+        match dir with
+        | "n" -> (x, y+1, z-1)
+        | "nw" -> (x-1, y+1, z)
+        | "ne" -> (x+1, y, z-1)
+        | "s" -> (x, y-1, z+1)
+        | "sw" -> (x-1, y, z+1)
+        | "se" -> (x+1, y-1, z)
+
+    let rec furthest (list: string list) coord distance =
+        match list with
+        | [] -> distance
+        | x::xt -> 
+            let (x, y , z) = nextCoord coord x
+            let dis = (Math.Abs x + Math.Abs y + Math.Abs z) / 2
+            furthest xt (x, y, z) (if dis > distance then dis else distance)
+
+    let answer =
+        let (x, y, z) = 
+            input
+            |> List.fold nextCoord (0, 0, 0)
+        (Math.Abs x + Math.Abs y + Math.Abs z) / 2
+
+    let answer2 =
+        furthest input (0, 0, 0) 0
+
+
